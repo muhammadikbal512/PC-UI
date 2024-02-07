a7ff2ca0f309951f4b740cc3a6f67617ddc2b2f9
Muhammad Iqbal <muhammadikbal511@gmail.com>
2024-02-07 15:17:57 +0700

    update dish component

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,6 +2,13 @@
 module.exports = {
   content: ["./src/**/*.{html,js}"],
   theme: {
+    screens: {
+      'xs': '320px',   // Example custom breakpoint for extra small screens
+      'sm': '640px',
+      'md': '768px',
+      'lg': '1024px',
+      'xl': '1280px',
+    },
     extend: {},
   },
   plugins: [],
