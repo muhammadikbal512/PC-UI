290a2a57488787447429d479610d15d335b999e6
Jans <janshenfu.y.t@gmail.com>
2024-03-14 14:54:50 +0700

    Merge branch 'main' of https://github.com/muhammadikbal512/PC-UI

--- src/app/feature/about/about.component.ts
+++ src/app/feature/about/about.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, ViewChild, HostListener  } from '@angular/core';
 
 @Component({
   selector: 'app-about',
@@ -8,5 +8,39 @@ import { Component } from '@angular/core';
   styleUrl: './about.component.css'
 })
 export class AboutComponent {
+  @ViewChild('VidCompro') videoPlayer!: ElementRef<HTMLVideoElement>;
+  private wasVideoPlaying = false;
+  constructor() { }
 
+  ngOnInit() {
+    document.addEventListener('visibilitychange', this.visibilityChangeHandler.bind(this));
+  }
+
+  ngOnDestroy() {
+    document.removeEventListener('visibilitychange', this.visibilityChangeHandler.bind(this));
+  }
+
+  visibilityChangeHandler() {
+    if (document.visibilityState === 'hidden') {
+      this.wasVideoPlaying = !this.videoPlayer.nativeElement.paused;
+      this.videoPlayer.nativeElement.pause(); // Menghentikan video saat tab menjadi tidak terlihat
+    } else if (document.visibilityState === 'visible' && this.wasVideoPlaying) {
+      this.videoPlayer.nativeElement.play(); // Memulai kembali video jika tab menjadi terlihat dan video sebelumnya sedang diputar
+    }
+  }
+
+  ngAfterViewInit() {
+    this.videoPlayer.nativeElement.volume = 0.2; 
+    const observer = new IntersectionObserver(entries => {
+      entries.forEach(entry => {
+        if (!entry.isIntersecting) {
+          this.videoPlayer.nativeElement.pause();
+        } else {
+          this.videoPlayer.nativeElement.play();
+        }
+      });
+    });
+  
+    observer.observe(this.videoPlayer.nativeElement);
+  }
 }
--- src/app/feature/menu/menu.component.ts
+++ src/app/feature/menu/menu.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, ViewChild, HostListener  } from '@angular/core';
 import { RouterModule } from '@angular/router';
 import { Router } from '@angular/router';
 
@@ -9,6 +9,42 @@ import { Router } from '@angular/router';
   templateUrl: './menu.component.html',
   styleUrl: './menu.component.css'
 })
-export class MenuComponent {
 
+export class MenuComponent {
+  @ViewChild('VidCompro') videoPlayer!: ElementRef<HTMLVideoElement>;
+  private wasVideoPlaying = false;
+  constructor() { }
+
+  ngOnInit() {
+    document.addEventListener('visibilitychange', this.visibilityChangeHandler.bind(this));
+  }
+
+  ngOnDestroy() {
+    document.removeEventListener('visibilitychange', this.visibilityChangeHandler.bind(this));
+  }
+
+  visibilityChangeHandler() {
+    if (document.visibilityState === 'hidden') {
+      this.wasVideoPlaying = !this.videoPlayer.nativeElement.paused;
+      this.videoPlayer.nativeElement.pause(); // Menghentikan video saat tab menjadi tidak terlihat
+    } else if (document.visibilityState === 'visible' && this.wasVideoPlaying) {
+      this.videoPlayer.nativeElement.play(); // Memulai kembali video jika tab menjadi terlihat dan video sebelumnya sedang diputar
+    }
+  }
+
+  ngAfterViewInit() {
+    this.videoPlayer.nativeElement.volume = 0.2; 
+    const observer = new IntersectionObserver(entries => {
+      entries.forEach(entry => {
+        if (!entry.isIntersecting) {
+          this.videoPlayer.nativeElement.pause();
+        } else {
+          this.videoPlayer.nativeElement.play();
+        }
+      });
+    });
+  
+    observer.observe(this.videoPlayer.nativeElement);
+  }
+  
 }
