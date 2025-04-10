0f044dd00e652ca677cbef8b144a309216c51a4e
gelaptaknampak <133974027+gelaptaknampak@users.noreply.github.com>
2025-04-10 10:31:41 +0700

    home revision

---
+++ src/app/feature/home/dashboard/component/berita/berita.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-berita',
+  standalone: true,
+  imports: [],
+  templateUrl: './berita.component.html',
+  styleUrl: './berita.component.css'
+})
+export class BeritaComponent {
+
+}
--- src/app/feature/home/dashboard/dashboard.component.ts
+++ src/app/feature/home/dashboard/dashboard.component.ts
@@ -4,14 +4,21 @@ import { DishComponent } from './component/dish/dish.component';
 import { WelcomeComponent } from './component/welcome/welcome.component';
 import { ChefComponent } from './component/chef/chef.component';
 import { TestimonialComponent } from './component/testimonial/testimonial.component';
-
+import { BeritaComponent } from './component/berita/berita.component';
+import { VideoComponent } from './component/video/video.component';
 @Component({
   selector: 'app-dashboard',
   standalone: true,
-  imports: [HeroComponent, DishComponent, WelcomeComponent, ChefComponent, TestimonialComponent],
+  imports: [
+    HeroComponent,
+    DishComponent,
+    WelcomeComponent,
+    ChefComponent,
+    TestimonialComponent,
+    BeritaComponent,
+    VideoComponent,
+  ],
   templateUrl: './dashboard.component.html',
-  styleUrl: './dashboard.component.css'
+  styleUrl: './dashboard.component.css',
 })
-export class DashboardComponent {
-
-}
+export class DashboardComponent {}
